2cf1149d1064252341d97435b9f55537d7d81e85
Sergei Slesarev <slesarev071290@gmail.com>
2020-04-03 11:55:29 +0300

    feat(challenge): sorted union

---
+++ challenges/sorted-union.js
@@ -0,0 +1,15 @@
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+// uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].
+// uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5].
+// uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].
+
+const uniteUnique = (...arr) =>
+  arr.reduce((acc, curr) => [
+    ...acc,
+    ...curr.filter(element => !acc.includes(element))
+  ]);
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
